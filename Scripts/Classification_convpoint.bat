58b630b7a90ce0b65a475a687f0e6e81bb66a5f8
pbugnet <pieroxyde@gmail.com>
2023-02-10 16:17:57 -0500

    cd

--- Scripts/Classification_convpoint.bat
+++ Scripts/Classification_convpoint.bat
@@ -1,2 +1,2 @@
-call conda activate pytorch_convpoint_p36 && start python.exe D:\GitHub\ConvPoint_Effigis\Scripts\Classification_ConvPoint.py
+call conda activate pytorch_convpoint_p36 && cd D:\GitHub\ConvPoint_Effigis\Scripts && start python.exe D:\GitHub\ConvPoint_Effigis\Scripts\Classification_ConvPoint.py
 pause
