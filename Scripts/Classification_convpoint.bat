d6b7dffa6630ba21adc62e2fd2d8428d2c768e5f
pbugnet <pieroxyde@gmail.com>
2023-02-23 15:51:19 -0500

    chemin complet de l'environnement

--- Scripts/Classification_convpoint.bat
+++ Scripts/Classification_convpoint.bat
@@ -1,2 +1,2 @@
-call conda activate pytorch_convpoint_p36 && cd D:\GitHub\ConvPoint_Effigis\Scripts && start python.exe D:\GitHub\ConvPoint_Effigis\Scripts\Classification_ConvPoint.py
+call conda activate C:\Users\pbug\.conda\envs\pytorch_convpoint_p36 && cd D:\GitHub\ConvPoint_Effigis\Scripts && start python.exe D:\GitHub\ConvPoint_Effigis\Scripts\Classification_ConvPoint.py
 pause
--- Scripts/Entrainement_convpoint.bat
+++ Scripts/Entrainement_convpoint.bat
@@ -1,1 +1,1 @@
-call conda activate pytorch_convpoint_p36 && cd D:\GitHub\ConvPoint_Effigis\Scripts && start python.exe D:\GitHub\ConvPoint_Effigis\Scripts\Entrainement_ConvPoint.py
+call conda activate C:\Users\pbug\.conda\envs\pytorch_convpoint_p36 && cd D:\GitHub\ConvPoint_Effigis\Scripts && start python.exe D:\GitHub\ConvPoint_Effigis\Scripts\Entrainement_ConvPoint.py
