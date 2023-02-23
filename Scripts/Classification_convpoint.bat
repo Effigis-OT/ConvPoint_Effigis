76f7ee73a6c96136c3a4aa431d7ea23b6fdad48a
pbugnet <pieroxyde@gmail.com>
2023-02-23 16:23:03 -0500

    ajout de D:

--- Scripts/Classification_convpoint.bat
+++ Scripts/Classification_convpoint.bat
@@ -1,2 +1,2 @@
-call conda activate C:\Users\pbug\.conda\envs\pytorch_convpoint_p36 && cd D:\GitHub\ConvPoint_Effigis\Scripts && start python.exe D:\GitHub\ConvPoint_Effigis\Scripts\Classification_ConvPoint.py
+call conda activate C:\Users\pbug\.conda\envs\pytorch_convpoint_p36 && D: && cd D:\GitHub\ConvPoint_Effigis\Scripts && start python.exe D:\GitHub\ConvPoint_Effigis\Scripts\Classification_ConvPoint.py
 pause
